14e440a96d6866839e0cc5fe036225cfbe68e884
David Brown <dmlb2000@gmail.com>
2015-10-07 20:36:34 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dmlb2000@gmail.com'
 license 'all_rights'
 description 'Installs/Configures dmlb2000_pipeline'
 long_description 'Installs/Configures dmlb2000_pipeline'
-version '0.1.3'
+version '0.1.4'
 
 depends 'git'

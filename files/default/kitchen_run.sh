dc0dc1899f8b120b9790b562d45032e2f5cbc146
David Brown <dmlb2000@gmail.com>
2016-01-20 22:30:31 -0800

    add some fixes to checkout instead of cd into the other place

--- files/default/kitchen_run.sh
+++ files/default/kitchen_run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -xe
 
-cd /var/lib/buildbot/slave/kitchen/${FROM_COOKBOOK}_kitchen/build
+if ! test -d ${FROM_COOKBOOK} ; then
+  mkdir -p ${FROM_COOKBOOK}
+  pushd ${FROM_COOKBOOK}
+  git init
+  git remote add origin http://github.com/dmlb2000/${FROM_COOKBOOK}
+  popd
+fi
+pushd ${FROM_COOKBOOK}
+git checkout ${REVISION}
 kitchen test
+popd
